7a7a8d623a477e4b34d6356a252025ea9bd698fc
amiga1114 <amiga1114@naver.com>
2018-01-12 08:00:19 +0000

    naverlogin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' } do
+  delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+ end
+
+ root 'posts#index'
+
+ resources :posts
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
