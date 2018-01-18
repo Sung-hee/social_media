44f7dab03b516266eb1823f992bf6d02d0d11ff7
Sung-hee <klpoik@naver.com>
2018-01-18 05:26:22 +0000

    hee_color change

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,22 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.all
-    # @posts = Post.where("tag LIKE ?", "%#{params["q"]}%") # 태그로 검색한거만 보여줌
+    # Post.where(['created_at < ?', 30.seconds.ago]).destroy_all
+    # Plus like
+    std_time = 30.seconds
+    plus_time = 10
+
+    post = Post.where(['created_at < ?', std_time.seconds.ago])
+
+    post.each do |post|
+      if post.likes != nil
+        like_count = post.likes.count
+        total_time = std_time + (like_count * plus_time)
+        Post.where(['created_at < ?', total_time.seconds.ago]).destroy_all
+      else
+        post.destroy
+      end
+    end
   end
 
   # GET /posts/1
