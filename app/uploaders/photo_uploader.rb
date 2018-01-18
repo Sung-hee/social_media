c76fe22c6a1c43829c776ac96b69c99f119136b1
Ubuntu <dlrudrhs3000@naver.com>
2018-01-18 13:13:40 +0000

    s3

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -6,16 +6,16 @@ class PhotoUploader < CarrierWave::Uploader::Base
   include ApplicationController::ApplicationHelper # 커런트유저 불러오기 위해 씀
 
   # Choose what kind of storage to use for this uploader:
-  storage :file # 로컬일때 활성화
-  # storage :fog # aws쓸 때 활성화
+  # storage :file # 로컬일때 활성화
+  storage :fog # aws쓸 때 활성화
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir # s3쓸 때 커맨트 아웃
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  # def store_dir # s3쓸 때 커맨트 아웃
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
 
-  # 버전 thumb 커맨트 아웃해야함
+  # 버전 thumb
   version :thumb do
     process resize_to_fit: [50, 50]
   end
